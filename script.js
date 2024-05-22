a8f21d8607884295b3164990f30a38029a99a0fc
Akim Zmerli <157289222+AkimZmerli@users.noreply.github.com>
2024-05-22 16:56:45 +0200

    add playGame function
    to dos: 
    
    fix function logic
    declare winner

--- script.js
+++ script.js
@@ -17,9 +17,7 @@ const computerChoice = getComputerChoice();
 
 console.log(`The mashines have chosen ${computerChoice}`);
 
-const humanChoice = prompt(
-  "Choose between `Rock`, `Paper` or `Scissors` now: "
-);
+
 
 function getHumanChoice() {
   return humanChoice;
@@ -32,44 +30,80 @@ const humanChoiceLower = humanChoice.toLowerCase();
 
 console.log(`You go with ${humanChoiceLower}`);
 
-let humanScore = 0; // Use let to allow updating the scores
-let computerScore = 0;
+let humanScore = 0; 
+    let computerScore = 0;
+let roundScore = 1;
 
-function playRound(humanChoiceLower, computerChoice) {
+function playRound(humanChoiceLower, computerChoice, roundScore) {
   // add round parameter
+  
+}
+
+function  playRound(humanChoice, computerChoice, roundScore)
+  
   if (humanChoice === "rock" && computerChoice === "rock") {
-    console.log("Unentschieden!");
-  } else if (humanChoiceLower === "rock" && computerChoice === "scissors") {
-    console.log("Rock beats Scissors! You win :)");
-    humanScore++;
-  } else if (humanChoiceLower === "rock" && computerChoice === "paper") {
-    console.log("Paper beats Rock...You lose!");
-    computerScore++;
-  } else if (humanChoiceLower === "paper" && computerChoice === "paper") {
-    console.log("Unentschieden!");
-  } else if (humanChoiceLower === "paper" && computerChoice === "scissors") {
-    console.log("Scissors beat Paper...You lose!");
-    computerScore++;
-  } else if (humanChoiceLower === "paper" && computerChoice === "rock") {
-    console.log("Paper beats Rock! You win :)");
-    humanScore++;
-  } else if (humanChoiceLower === "scissors" && computerChoice === "rock") {
-    console.log("Rock beats Scissors! You lose :(");
-    computerScore++;
-  } else if (humanChoiceLower === "scissors" && computerChoice === "scissors") {
-    console.log("Unentschieden!");
-  } else if (humanChoiceLower === "scissors" && computerChoice === "paper") {
-    console.log("Scissors beat Paper! You win :)");
-    humanScore++;
-  } else {
-    console.log("Invalid input!!!!");
+  console.log("Unentschieden!");
+  roundScore++;
+} else if (humanChoiceLower === "rock" && computerChoice === "scissors") {
+  console.log("Rock beats Scissors! You win :)");
+  humanScore++;
+  roundScore++;
+} else if (humanChoiceLower === "rock" && computerChoice === "paper") {
+  console.log("Paper beats Rock...You lose!");
+  computerScore++;
+  roundScore++;
+} else if (humanChoiceLower === "paper" && computerChoice === "paper") {
+  console.log("Unentschieden!");
+  roundScore++;
+} else if (humanChoiceLower === "paper" && computerChoice === "scissors") {
+  console.log("Scissors beat Paper...You lose!");
+  computerScore++;
+  roundScore++;
+} else if (humanChoiceLower === "paper" && computerChoice === "rock") {
+  console.log("Paper beats Rock! You win :)");
+  humanScore++;
+  roundScore++;
+} else if (humanChoiceLower === "scissors" && computerChoice === "rock") {
+  console.log("Rock beats Scissors! You lose :(");
+  computerScore++;
+  roundScore++;
+} else if (humanChoiceLower === "scissors" && computerChoice === "scissors") {
+  console.log("Unentschieden!");
+  roundScore++;
+} else if (humanChoiceLower === "scissors" && computerChoice === "paper") {
+  console.log("Scissors beat Paper! You win :)");
+  humanScore++;
+  roundScore++;
+} else {
+  console.log("Invalid input!!!!");
+}
+
+playRound(humanChoiceLower, computerChoice, roundScore);
+
+console.log(
+  `This has been round # ${roundScore}! Our scoreboard tells us: ${humanScore} for you and ${computerScore} for the mashines!`
+);
+
+
+function playGame(humanScore, computerScore, roundScore) {
+  for (let roundScore = 1; roundScore <= 5; roundScore++) {
+    const result = playRound(humanChoice, computerChoice, roundScore);
+    const humanChoice = prompt(
+      "Choose between `Rock`, `Paper` or `Scissors` now: "
+    );
+    const computerChoice = getComputerChoice();
+    
+  }
+  if (humanScore >= 3) {
+    return console.log("You made the mashines suffer! Nice one by winning the game :)");
+    
+  }
+  else if (computerScore >= 3) {
+    return console.log("The mashines have won. We shall submit and let them take over. Thanks you fool..");
+  }
+  else {
+    return console.log("Its a tie!!! KEEEP fighting!")
   }
 }
 
-playRound(humanChoiceLower, computerChoice);
-
-console.log(
-  `The scoreboard says: ${humanScore} for you and ${computerScore} for the mashines!`
-);
-
-// function playGame() {}  add function to play five rounds
+playGame();
